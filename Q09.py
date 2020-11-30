050d5b12a1d638ddc6d1aa4b3ad520d130d18a29
caraujo-geo <58032077+caraujo-geo@users.noreply.github.com>
2020-11-30 12:02:44 -0300

    Update Q09.py

--- Q09.py
+++ Q09.py
@@ -1,6 +1,9 @@
 '''9. supondo um retângulo de  10cm de base e 5cm de altura, imprimir a seguinte saída perímetro:/ área: / diagonal:'''
 base = 10
 altura = 5
-perimetro = 0
-area = 0
-diagonal = 0
+perimetro = 2*(base+altura)
+area = base*altura
+diagonal = (((base**2)+(altura**2))**(1/2))
+
+
+print ('Perimetro =',perimetro,'\nÁrea =' ,area,'\nDiagonal =' ,diagonal)
